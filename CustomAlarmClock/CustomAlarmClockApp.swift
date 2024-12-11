d6efcda1e220e3c9b2d36aeedf54270b2f37dd5c
Francisco Alberdi <francisco_alberdi@bellsouth.net>
2024-12-11 13:12:25 -0500

    Initial commit

---
+++ CustomAlarmClock/CustomAlarmClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomAlarmClockApp.swift
+//  CustomAlarmClock
+//
+//  Created by Francisco Alberdi on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomAlarmClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
