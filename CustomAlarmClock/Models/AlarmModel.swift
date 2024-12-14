3fcda79b07cf5671d0c549e6bf1961c5c1696b8d
D-Astudillo-ASC <dma3@williams.edu>
2024-12-13 21:39:30 -0500

    Renaming Alarm.swift to AlarmModel and moving to Models directory.

--- CustomAlarmClock/Models/AlarmModel.swift
+++ CustomAlarmClock/Models/AlarmModel.swift
@@ -1,5 +1,5 @@
 //
-//  Alarm.swift
+//  AlarmModel.swift
 //  CustomAlarmClock
 //
 //  Created by Francisco Alberdi on 12/11/24.
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Alarm: Identifiable {
+struct AlarmModel: Identifiable {
     var id = UUID()
     var time: Date
     var label: String
